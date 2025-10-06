bee79a722bd98bac5e7beb69b912e0be7f05bcd5
Dhinesh <dhinesh342@gmail.com>
2025-10-06 10:29:15 +0530

    Hour minute

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(58);
+  const [minute,setMinute]= useState(60);
+  const [hour,setHour]= useState(0);
 
   useEffect(()=>{
     let timer=setInterval(()=>{
       setCount((prev)=>prev+1);
     },1000)
+
+    if(count>60){
+      setMinute((prev)=>prev+1);
+      setCount(0)
+    }
+
+    if(minute>60){
+      setHour((prev)=>prev+1);
+      setMinute(0)
+    }
     
     return ()=>clearInterval(timer);
   },[count])
 
+
   return (
     <>
-      <span>{count}</span>
+      <span>{hour}:{minute}:{count}</span>
     </>
   )
 }
